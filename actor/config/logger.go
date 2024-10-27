fb28823e1032d71163b3cfd829f47985de671c03
jpcairesf <jpcairesf@gmail.com>
2024-10-27 19:44:15 -0300

    refactor: logger structure more simple

--- actor/config/logger.go
+++ actor/config/logger.go
@@ -16,35 +16,19 @@ type Logger struct {
 	writer io.Writer
 }
 
-func (l *Logger) Debug(v ...interface{}) {
-	l.debug.Println(v...)
-}
-
-func (l *Logger) Info(v ...interface{}) {
-	l.info.Println(v...)
-}
-
-func (l *Logger) Warn(v ...interface{}) {
-	l.warn.Println(v...)
-}
-
-func (l *Logger) Error(v ...interface{}) {
-	l.error.Println(v...)
-}
-
-func (l *Logger) Debugf(format string, v ...interface{}) {
+func (l *Logger) Debugf(format string, v ...any) {
 	l.debug.Printf(format, v...)
 }
 
-func (l *Logger) Infof(format string, v ...interface{}) {
+func (l *Logger) Infof(format string, v ...any) {
 	l.info.Printf(format, v...)
 }
 
-func (l *Logger) Warnf(format string, v ...interface{}) {
+func (l *Logger) Warnf(format string, v ...any) {
 	l.warn.Printf(format, v...)
 }
 
-func (l *Logger) Errorf(format string, v ...interface{}) {
+func (l *Logger) Errorf(format string, v ...any) {
 	l.error.Printf(format, v...)
 }
 
